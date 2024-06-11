3033ffd86f4e71a5cbf428d680692ecc1ee6b7aa
Bideshsaha <sahabidesh523@gmail.com>
2024-06-11 16:53:31 +0530

    age checking using php done

---
+++ html/vote.php
@@ -0,0 +1,33 @@
+<?php
+    
+    // $age=$_POST["age"];
+
+    // if(array_key_exists('button', $_POST)) { 
+    //     check($age); 
+    // } 
+
+ 
+
+
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $age = $_POST["age"];
+
+        function check($age) {
+            if ($age <= 0) {
+                echo "Please enter a valid age.";
+            } elseif ($age < 18) {
+                echo "You are not eligible for voting.";
+            } else {
+                echo "You are eligible for voting.";
+            }
+        }
+
+        check($age);
+    }
+?>
+
+
+
+  
---
+++ html/votephp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vote condition</title>
+    <link rel="stylesheet" href="vote.css">
+    
+    
+</head>
+<body>
+    
+    <div class="container">
+        
+        <form   method="post">
+            <label>Age</label>
+            <input type="number" name="age" placeholder="Enter your Age" id="age">
+            <input type="submit" name="button" value="Check" id="btn">
+            
+            
+        </form>
+        <div class="display-section">
+            <label id="title">Validity</label>
+            <div id="validity-displaybox">
+                <?php include 'vote.php';?>
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+</html>
