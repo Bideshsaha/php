5f9f0fdece7c4b7513ff7aa5b956d620131b249d
Bideshsaha <sahabidesh523@gmail.com>
2024-07-02 20:18:44 +0530

    login using one click function

--- html/JS/loginpage.js
+++ html/JS/loginpage.js
@@ -1,15 +1,24 @@
-console.log("loaded home js");
+console.log("loaded home");
+
+// $(document).ready(function(){
+//     $("#open").hide();
+//     $("#close").click(function(){
+//         $("#close").hide();
+//         $("#open").show();
+//         $('#psw').attr('type', 'text');
+//     });
+//     $("#open").click(function(){
+//         $("#open").hide();
+//         $("#close").show();
+//         $('#psw').attr('type', 'password');
+//     });
+// });
 
 $(document).ready(function(){
     $("#open").hide();
-    $("#close").click(function(){
-        $("#close").hide();
-        $("#open").show();
-        $('#psw').attr('type', 'text');
-    });
-    $("#open").click(function(){
-        $("#open").hide();
-        $("#close").show();
-        $('#psw').attr('type', 'password');
+    $("#close,#open").click(function(){
+        $("#close, #open").toggle();
+    var type = $('#psw').attr('type') === 'password' ? 'text' : 'password';
+    $('#psw').attr('type', type);
     });
 });
