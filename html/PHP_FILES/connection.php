d2b2e6a82ae44cdb0a9cde15f0fb2dba049a54cc
Bideshsaha <sahabidesh523@gmail.com>
2024-07-11 15:59:13 +0530

    unique email verification code implemented

--- html/PHP_FILES/connection.php
+++ html/PHP_FILES/connection.php
@@ -6,9 +6,8 @@ $password = "";
 $database = "blog";
 
 $conn = mysqli_connect($host,$user_name,$password,$database);
-if(!$conn){
-    echo 'connection is not done.';
-}else{
-    echo 'connection done sucessfully';
-}
-$query = mysqli_query($conn,"INSERT INTO `users` (`fname`, `lname`, `email`, `password`, `cpassword`) VALUES ('Bidesh', 'Saha', 'sahabidesh523@gmail.com', 'Bidesh@124', 'Bidesh@124')");
+// if(!$conn){
+//     echo 'connection is not done.';
+// }else{
+//     echo 'connection done sucessfully';
+// }
--- html/PHP_FILES/signup.php
+++ html/PHP_FILES/signup.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'connection.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $fName = $_POST["fn"];
     $lName = $_POST["ln"];
@@ -12,6 +12,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     }else if($len>=11){
         echo 'Not possible! please enter 8-10 charecter';
     }else{
-        echo 'Signed up successfully';
+
+        if($Email != "") {
+            $result = mysqli_query($conn,"SELECT * FROM users where email='".$Email."'");
+            $num_rows = mysqli_num_rows($result);
+            if($num_rows >= 1){
+                echo "email exist";
+            }
+            else{
+                echo 'Signed up successfully';
+                $query = mysqli_query($conn,"INSERT INTO `users` (`fname`, `lname`, `email`, `password`, `cpassword`) 
+                VALUES ('$fName', '$lName', '$Email', '$pass', '')");
+            }
+        }
+
+
+        
     }
 }
